95edd69abb63630f84b078952dd36abf71b18ac5
Ghoi Ga Hui <cherrycoke@sookmyung.ac.kr>
2023-04-04 10:13:43 +0900

    프론트 개발 10. React로 목록 추가, 수정, 삭제하기

--- src/App.js
+++ src/App.js
@@ -17,14 +17,24 @@ class App extends React.Component{
     };
   }
 
-  // (1) add 함수 추가
+  // add 함수 추가
   add = (item) => {
     const thisItems = this.state.items;
     item.id = "ID-" + thisItems.length; // key를 위한 id 추가
     item.done = false;
-    thisItems.pust(item);
+    thisItems.push(item);
     this.setState({ items: thisItems }); // update state
     console.log("items:", this.state.items);
+  }
+
+  // delete 함수 추가 (alt + shift + f 포맷팅)
+  delete = (item) => {
+    const thisItems = this.state.items;
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
+      // 디버깅 콜백
+      console.log("Update Items: ", this.state.items)
+    });
   }
   
   render() {
@@ -34,14 +44,14 @@ class App extends React.Component{
       <Paper style={{margin:16}}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} />
+            <Todo item={item} key={item.id} delete={this.delete} />
           ))}
         </List>
       </Paper>
     );
     
     
-    // (2) add 함수 연결
+    // add 함수 연결
     return (
       <div className="App">
         <Container maxWidth="md">
